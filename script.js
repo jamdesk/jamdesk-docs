9ac77a91cddbc05328dc6e175b00ffdaa8e6ff74
Geoffrey Bourne <gbourne1@users.noreply.github.com>
2026-03-04 23:33:47 -0500

    feat: add Crisp chat widget script
    Auto-detected by Jamdesk custom JavaScript support.

---
+++ script.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "03c4dcd2-291c-44fe-9fe3-8c90c502fc3f";
+(function () {
+  var d = document;
+  var s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
